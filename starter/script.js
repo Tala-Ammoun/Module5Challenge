5f31562c2091dc71ed8f0aa151749a12724803ff
Tala Ammoun <talaammoun@gmail.com>
2023-01-06 16:04:59 +0000

    I was able to confirm user choice and concatenate arrays into one big one based on that choice. I still need to generate random characters based on the password length the user chose.

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,5 @@
 // Array of special characters to be included in password
-var specialCharacters = [
+let specialCharacters = [
   '@',
   '%',
   '+',
@@ -26,10 +26,10 @@ var specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+let lowerCasedCharacters = [
   'a',
   'b',
   'c',
@@ -59,7 +59,7 @@ var lowerCasedCharacters = [
 ];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
+let upperCasedCharacters = [
   'A',
   'B',
   'C',
@@ -88,31 +88,63 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
+let bigArray = [] 
 
-}
+let passwordLength = prompt("Choose how long you would like your password to be from 10 to 64 characters.")
 
 // Function for getting a random element from an array
-function getRandom(arr) {
+function getRandom() {
+  let randomSpecOption = confirm("Would you like your password to have special characters: @%$!*&?")
+  if (randomSpecOption == true){
+    bigArray = bigArray.concat(specialCharacters)}
 
+  let randomNumbOption = confirm("Would you like your password to have numbers?")
+  if (randomNumbOption == true){
+    bigArray = bigArray.concat(numericCharacters)}
+  
+  let randomLowerOption = confirm("Would you like your password to have lower case letters?")
+  if (randomLowerOption == true){
+    bigArray = bigArray.concat(lowerCasedCharacters)}
+
+  let randomUpperOption = confirm("Would you like your password to have upper case letters?")
+  if (randomUpperOption == true){
+    bigArray = bigArray.concat(upperCasedCharacters)}
+    
+    return bigArray
 }
+getRandom()
 
 // Function to generate password with user input
+let passwordGenerator = Math.floor(Math.random()*bigArray.length)
+let practicePassword = "";
 function generatePassword() {
-
+  for (let i = 0; i < passwordLength; i++) {
+    practicePassword += passwordGenerator
 }
+return practicePassword
+}
+console.log(generatePassword())
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+  passwordText.value = password; //taking an array of characters and turning it to a string (.join)
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
+
+//concat is good for combining arrays into one.
+//push is good for adding a character to the back of an array.
+//map is used in for loop.
+//create a new array that cancatenates what the user wants in their password. Then generate password with the characters they chose.
+// the password will change everytime they click generate password, but the characters will stay the same. 
